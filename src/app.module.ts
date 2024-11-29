b0a108c8d1bb23d75bd7f290ff993c66d03d045b
DDCT <dylanclerque_2003@hotmail.com>
2024-11-29 18:41:55 -0500

    Back

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { RopaModule } from './ropa/ropa.module';
+import { WeatherModule } from './weather/weather.module';
+import { ColorModule } from './color/color.module';
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ropacore:ropacore@cluster0.mi2yl.mongodb.net/ropacoredb?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    UserModule,
+    RopaModule,
+    WeatherModule,
+    ColorModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/ropa/ropa.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Ropa } from './schemas/ropa.schema';
+
+@Injectable()
+export class RopaService {
+  constructor(@InjectModel(Ropa.name) private ropaModel: Model<Ropa>) {}
+
+
+  async create(ropa: any){
+    const newRopa = new this.ropaModel(ropa);
+    return await newRopa.save();
+  }
+
+  async findAll(): Promise<Ropa[]> {
+    return this.ropaModel.find().exec();
+  }
+
+   // Editar prenda
+   async update(id: string, updateRopa: any): Promise<Ropa> {
+    return this.ropaModel.findByIdAndUpdate(id, updateRopa, { new: true }).exec();
+  }
+
+  async searchOne(id: string): Promise<Ropa> {
+    return this.ropaModel.findById(id).exec();
+  }
+
+  // Eliminar prenda
+  async delete(id: string): Promise<Ropa> {
+    return this.ropaModel.findByIdAndDelete(id).exec();
+  }
+}
