197a8e8de88dc407e69c3b502f9f35786410c936
DDCT <dylanclerque_2003@hotmail.com>
2024-11-29 23:39:51 -0500

    NoDis

--- dist/weather/weather.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { WeatherService } from './weather.service';
-import { Ropa } from '../ropa/schemas/ropa.schema';
-export declare class WeatherController {
-    private readonly weatherService;
-    constructor(weatherService: WeatherService);
-    getClothes(city: string): Promise<Ropa[]>;
-    getClothesWFA(formalidad: string, edad: number): Promise<Ropa[]>;
-}
